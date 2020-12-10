19423df6ad1532d826fee4b8b25bb423baedb53e
Bruno Molina <brmolina@gmail.com>
2020-12-10 20:08:08 -0300

    Disparando eventos customizados

--- estado-e-cidade/ajax-e-promises.js
+++ estado-e-cidade/ajax-e-promises.js
@@ -1,5 +1,38 @@
+var estado = (function() {
+	var comboEstado = $('#combo-estado');
 
-$(function() {
+	function iniciar() {
+		$.ajax({
+		url: 'http://localhost:8080/estados',
+		method: 'GET',
+		dataType: 'json',
+		success: onEstadosRetornados,
+		error: onError
+	});
+	}
+
+	function onEstadosRetornados(estados) {
+		comboEstado.html('<option>Selecione o Estado</option>');
+
+		estados.forEach(function(estado) {
+			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+			comboEstado.append(optionEstado);
+		});
+	};
+
+	function onError() {
+		resposta.fail(function (errorctx){ console.log('erro na requisição',errorctx);});
+	};
+
+	return {
+		iniciar: iniciar
+	}
+})();
+
+estado.iniciar();
+
+/*$(function() {
+
 
 
 	var resposta = $.ajax({
@@ -11,7 +44,7 @@ $(function() {
 
 	resposta.done(function(estados) {
 
-		var comboEstado = $('#combo-estado')
+		
 		comboEstado.html('<option>Selecione o Estado</option>');
 
 		estados.forEach(function(estado) {
@@ -22,5 +55,4 @@ $(function() {
 
 	resposta.fail(function (errorctx){ console.log('erro na requisição',errorctx);});
 
-});
-
+});*/
